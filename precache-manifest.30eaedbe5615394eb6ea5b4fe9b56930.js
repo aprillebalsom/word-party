9f329cc7c70d8863243367ec741d3c69e1d9c97e
aprillebalsom <aprillebalsom@gmail.com>
2020-10-18 11:56:36 -0400

    Updates

---
+++ precache-manifest.30eaedbe5615394eb6ea5b4fe9b56930.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9d9819ebba165808b4ae89ca898849",
+    "url": "/word-party/index.html"
+  },
+  {
+    "revision": "ffda36678e64d306bc95",
+    "url": "/word-party/static/css/main.892350cd.chunk.css"
+  },
+  {
+    "revision": "7c36cec45f695289635f",
+    "url": "/word-party/static/js/2.0e03b05a.chunk.js"
+  },
+  {
+    "revision": "16056f19fc2a7e8fef3e22f99eb947d7",
+    "url": "/word-party/static/js/2.0e03b05a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffda36678e64d306bc95",
+    "url": "/word-party/static/js/main.404dcb2e.chunk.js"
+  },
+  {
+    "revision": "3014f6e03fc8dd1b21ee",
+    "url": "/word-party/static/js/runtime-main.6fbf841f.js"
+  },
+  {
+    "revision": "a16074b58d90392e499a3e961a577ed3",
+    "url": "/word-party/static/media/looseLeafBckgnd.a16074b5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wordParty/precache-manifest.80fbd121a810c51718bcffef8f72720c.js"
+  "/word-party/precache-manifest.30eaedbe5615394eb6ea5b4fe9b56930.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wordParty/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/word-party/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
